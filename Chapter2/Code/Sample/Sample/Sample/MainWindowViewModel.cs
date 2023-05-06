fb33f4fbc889b939c3fbe052fb763521e1b28ee1
Geroshabu <oonstks@gmail.com>
2023-05-06 18:15:35 +0900

    コマンドの基本のサンプルコード

---
+++ Chapter2/Code/Sample/Sample/Sample/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Input;
+
+namespace Sample
+{
+    /// <summary>
+    /// MainWindow に対するビューモデル
+    /// </summary>
+    internal class MainWindowViewModel
+    {
+        /// <summary>
+        /// ボタンが押されたときのコマンド
+        /// </summary>
+        public ICommand DoSomethingCommand { get; } = new ShowMessageCommand();
+    }
+}
---
+++ Chapter2/Code/Sample/Sample/Sample/ShowMessageCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Sample
+{
+    /// <summary>
+    /// メッセージを表示するコマンド
+    /// </summary>
+    internal class ShowMessageCommand : ICommand
+    {
+        /// <inheritdoc cref="ICommand.CanExecuteChanged"/>
+        public event EventHandler? CanExecuteChanged;
+
+        /// <inheritdoc cref="ICommand.CanExecute(object?)"/>
+        public bool CanExecute(object? parameter) => true;
+
+        /// <inheritdoc cref="ICommand.Execute(object?)"/>
+        public void Execute(object? parameter)
+        {
+            MessageBox.Show("ボタンが押されました。");
+        }
+    }
+}
