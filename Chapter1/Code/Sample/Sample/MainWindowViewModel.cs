51204aabb23d0342463c4b22b3cd946bf01e5895
Geroshabu <oonstks@gmail.com>
2023-05-06 17:21:10 +0900

    WPFの基本のサンプルコード

---
+++ Chapter1/Code/Sample/Sample/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Sample
+{
+    /// <summary>
+    /// <see cref="MainWindow"/>に対応するビューモデル。
+    /// </summary>
+    internal class MainWindowViewModel
+    {
+        private readonly User model = new();
+
+        /// <summary>名前</summary>
+        public string Name
+        {
+            get
+            {
+                return model.Name;
+            }
+        }
+
+        /// <summary>年齢</summary>
+        public int Age
+        {
+            get
+            {
+                return model.Age;
+            }
+        }
+    }
+}
---
+++ Chapter1/Code/Sample/Sample/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Sample
+{
+    /// <summary>
+    /// 一人のユーザーの情報
+    /// </summary>
+    internal class User
+    {
+        /// <summary>名前</summary>
+        public string Name { get; } = "Freddie";
+
+        /// <summary>年齢</summary>
+        public int Age { get; } = 42;
+    }
+}
